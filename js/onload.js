4c1cbb4575483eb09bbc4e693eb688cabdeeb769
orpheusown <orpheusown@gmail.com>
2016-08-31 12:29:05 +0800

    修复若干bug

--- js/onload.js
+++ js/onload.js
@@ -29,17 +29,18 @@ jQuery(document).ready(function($) {
 	    var order=parseInt($(window).scrollTop()/window.innerHeight);
 	    tab(order);
 
-	for (var i=0;i<$(".main_scroll div").length;i++)
-	{
-		$(".main_scroll div")[i].onclick=(function(ii){
-
-			return function(){
-				window.scrollTo(0,window.innerHeight*ii);
-				tab(ii);
-			}
-		})(i);
-	}
 	});
+
+	for (var i=0;i<$(".main_scroll div").length;i++)//给所有右边scroll小圆绑定事件
+		{
+			$(".main_scroll div")[i].onclick=(function(ii){
+
+				return function(){
+					window.scrollTo(0,window.innerHeight*ii);
+					tab(ii);
+				}
+			})(i);
+		}
 
 	var oNav=$(".nav")[0];
 	var oMove=$(".nav_bg")[0];
@@ -57,8 +58,11 @@ jQuery(document).ready(function($) {
 			{
 					oNavA[i].style.color="#999";
 			}
-			$(".nav_login")[0].style.background="url(images/loginhover.png) no-repeat";
-			$(".nav_login")[0].style.backgroundSize="70%";
+			if($(".nav_login")[0]){
+				$(".nav_login")[0].style.background="url(images/loginhover.png) no-repeat";
+				$(".nav_login")[0].style.backgroundSize="70%";
+			}
+			$(".nav_logined img").attr("src","images/logged_checked.png");
 		});
 	}
 	oNav.onmouseout=function(){
@@ -68,30 +72,35 @@ jQuery(document).ready(function($) {
 			{
 					oNavA[i].style.color="#fff";
 			}
-			$(".nav_login")[0].style.background="url(images/login.png) no-repeat";
-			$(".nav_login")[0].style.backgroundSize="70%";
+			if($(".nav_login")[0]){
+				$(".nav_login")[0].style.background="url(images/login.png) no-repeat";
+				$(".nav_login")[0].style.backgroundSize="70%";
+			}
+			$(".nav_logined img").attr("src","images/logged.png");
 		});
 	}
 
-	$("#nav_login_a").on('click', function(event) {
-		event.preventDefault();
-		/* Act on the event */
-		$(".login")[0].style.display="block";
-	});
-	$(".login_close").on('click', function(event) {
-		event.preventDefault();
-		/* Act on the event */
-		$(".login")[0].style.display="none";
-		$(".sign_open")[0].style.display="none";
-	});
-	$("#sign_open_in").on('click', function(event) {
-		event.preventDefault();
-		/* Act on the event */
-		$(".sign_open")[0].style.display="block";
-	});
-	$("#login_open_in").on('click', function(event) {
-		event.preventDefault();
-		/* Act on the event */
-		$(".sign_open")[0].style.display="none";
-	});
+	if($("#nav_login_a")){
+		$("#nav_login_a").on('click', function(event) {
+			event.preventDefault();
+			/* Act on the event */
+			$(".login")[0].style.display="block";
+		});
+		$(".login_close").on('click', function(event) {
+			event.preventDefault();
+			/* Act on the event */
+			$(".login")[0].style.display="none";
+			$(".sign_open")[0].style.display="none";
+		});
+		$("#sign_open_in").on('click', function(event) {
+			event.preventDefault();
+			/* Act on the event */
+			$(".sign_open")[0].style.display="block";
+		});
+		$("#login_open_in").on('click', function(event) {
+			event.preventDefault();
+			/* Act on the event */
+			$(".sign_open")[0].style.display="none";
+		});
+	}
 });
